5e210873e19d88360f0643f1a491dc45497a2c10
Samuel Satizabal <samuelsatizabal@hotmail.com>
2024-01-14 18:11:38 -0500

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,6 @@ import Login from "./components/pages/Login.tsx";
 
 const router = createHashRouter([
   {
-    path: "/",
-    element: <p>login</p>,
-  },
-  {
     path: "/log",
     element: <Login />,
   },
