261b343815c8d6cb914c3ba51aece79ce743e161
Samuel Satizabal <samuelsatizabal@hotmail.com>
2024-01-14 17:09:42 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/custom-components/Layout";
 import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/holafly-front/" element={<Layout />}>
-        <Route path="" element={<Home />} />
-        <Route path="/holafly-front/home" element={<Home />} />
+      <Route path="/Holafly-front/" element={<Layout />}>
+        <Route path="" element={<Login />} />
+        <Route path="/Holafly-front/home" element={<Home />} />
       </Route>
     </Routes>
   );
