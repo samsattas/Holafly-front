f0bc07eb1df6fcf78ffcbe380720102ba109cef1
Samuel Satizabal <samuelsatizabal@hotmail.com>
2024-01-14 17:52:23 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Layout from "./components/custom-components/Layout";
-import Home from "./components/pages/Home";
-import Login from "./components/pages/Login";
+// import Layout from "./components/custom-components/Layout";
+// import Home from "./components/pages/Home";
+// import Login from "./components/pages/Login";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <Routes>
-      <Route path="/Holafly-front/" element={<Layout />}>
-        <Route path="" element={<Login />} />
-        <Route path="/Holafly-front/home" element={<Home />} />
-      </Route>
-    </Routes>
+    // <Routes>
+    //   <Route path="/Holafly-front/" element={<Layout />}>
+    //     <Route path="" element={<Login />} />
+    //     <Route path="/Holafly-front/home" element={<Home />} />
+    //   </Route>
+    // </Routes>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank"></a>
+        <a href="https://react.dev" target="_blank"></a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createHashRouter } from "react-router-dom";
-import Home from "./components/pages/Home.tsx";
-import Login from "./components/pages/Login.tsx";
+// import { RouterProvider, createHashRouter } from "react-router-dom";
+// import Home from "./components/pages/Home.tsx";
+// import Login from "./components/pages/Login.tsx";
+import App from "./App.tsx";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <p>login</p>,
-  },
-  {
-    path: "/log",
-    element: <Login />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-  },
-]);
+// const router = createHashRouter([
+//   {
+//     path: "/",
+//     element: <p>login</p>,
+//   },
+//   {
+//     path: "/log",
+//     element: <Login />,
+//   },
+//   {
+//     path: "/home",
+//     element: <Home />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
